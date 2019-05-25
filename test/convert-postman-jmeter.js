398abd2277013a814c452f041eadf77c56eca0d2
Sergio <chancaysergio@gmail.com>
2019-05-24 20:03:33 -0500

    initialize project

---
+++ test/convert-postman-jmeter.js
@@ -0,0 +1,27 @@
+var convertPostmanJmeter = require('../lib/convert-postman-jmeter.js');
+var expect = require('chai').expect;
+
+describe('Constructor', function() {
+  context('validating options', function() {
+    it('without options', function() {
+      expect(convertPostmanJmeter.convert()).to.equal(false);
+    });
+
+    it('with file empty value on options', function() {
+      expect(convertPostmanJmeter.convert({projectPostman: ''})).to.equal(false);
+    });
+
+    it('without file project postman not exists on options', function() {
+      expect(convertPostmanJmeter.convert({projectPostman: 'not-exists.json'})).to.equal(false);
+    });
+
+    it('with file project postman exists on options', function() {
+      const options = {
+        projectPostman: 'test/resources/test-api-without-environments.postman_collection.json',
+        projectJmeter: 'test/resources/test-api-without-environments.postman_collection.jmx',
+        override: true
+    };
+      expect(convertPostmanJmeter.convert(options)).to.equal(true);
+    });
+  });
+});
