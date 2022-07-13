43fe8546df62d7d6f3df34f650ce8f63b7411e45
sercheo87 <vAcpe8-gufnup-tentet>
2022-07-12 19:47:55 -0500

    Fix project configuration #83

--- test/convert-postman-jmeter.js
+++ test/convert-postman-jmeter.js
@@ -6,15 +6,19 @@ describe('Constructor', function() {
   context('validating options', function() {
     const jmeterProjectFile1 = 'test/resources/test-api-with-out-request.postman_collection.jmx';
     const jmeterProjectFile2 = 'test/resources/test-api-without-environments.postman_collection.jmx';
+    const jmeterProjectFile3 = 'test/resources/test-api-without-environments-v9.21.5.postman_collection.jmx';
     const file1 = 'test/resources/batch-projects/test-api-without-environments-1.postman_collection.jmx';
     const file2 = 'test/resources/batch-projects/test-api-without-environments-2.postman_collection.jmx';
 
-    beforeEach(function() {
+    before(function() {
       if (fs.existsSync(jmeterProjectFile1)) {
         fs.unlinkSync(jmeterProjectFile1);
       }
       if (fs.existsSync(jmeterProjectFile2)) {
         fs.unlinkSync(jmeterProjectFile2);
+      }
+      if (fs.existsSync(jmeterProjectFile3)) {
+        fs.unlinkSync(jmeterProjectFile3);
       }
       if (fs.existsSync(file1)) {
         fs.unlinkSync(file1);
@@ -24,12 +28,15 @@ describe('Constructor', function() {
       }
     });
 
-    afterEach(function() {
+    after(function() {
       if (fs.existsSync(jmeterProjectFile1)) {
         fs.unlinkSync(jmeterProjectFile1);
       }
       if (fs.existsSync(jmeterProjectFile2)) {
         fs.unlinkSync(jmeterProjectFile2);
+      }
+      if (fs.existsSync(jmeterProjectFile3)) {
+        fs.unlinkSync(jmeterProjectFile3);
       }
       if (fs.existsSync(file1)) {
         fs.unlinkSync(file1);
@@ -60,6 +67,15 @@ describe('Constructor', function() {
       expect(convertPostmanJmeter.convert(options)).to.equal(true);
     });
 
+    it('with file project postman exists on options new version Postman v9.21.5', function() {
+      const options = {
+        projectPostman: 'test/resources/test-api-without-environments-v9.21.5.postman_collection.json',
+        projectJmeter: jmeterProjectFile3,
+        override: true,
+      };
+      expect(convertPostmanJmeter.convert(options)).to.equal(true);
+    });
+
     it('with file project postman without request', function() {
       const options = {
         projectPostman: 'test/resources/test-api-with-out-request.postman_collection.json',
@@ -68,18 +84,18 @@ describe('Constructor', function() {
       };
 
       expect(convertPostmanJmeter.convert(options)).to.equal(true);
-      expect(fs.existsSync(jmeterProjectFile1)).to.equal(true);
+      expect(fs.existsSync(jmeterProjectFile1)).to.equal(false);
     });
 
     it('with folder batch project generate jmeter projects', function() {
+      const file1 = 'test/resources/batch-projects/test-api-without-environments-1.postman_collection.jmx';
+      const file2 = 'test/resources/batch-projects/test-api-without-environments-2.postman_collection.jmx';
+
       const options = {
         batchFolder: 'test/resources/batch-projects',
       };
 
       expect(convertPostmanJmeter.convert(options)).to.equal(true);
-
-      const file1 = 'test/resources/batch-projects/test-api-without-environments-1.postman_collection.jmx';
-      const file2 = 'test/resources/batch-projects/test-api-without-environments-2.postman_collection.jmx';
 
       expect(fs.existsSync(file1)).to.equal(true);
       expect(fs.existsSync(file2)).to.equal(true);
