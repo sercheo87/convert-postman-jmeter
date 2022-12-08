bc9a30c71774f126a142bbe5c62114e41b05afda
sercheo87 <vAcpe8-gufnup-tentet>
2022-12-07 22:58:51 -0500

    feat(Utility): Service
    - Add support for environment file
    
    BREAKING CHANGE: #94

--- test/convert-postman-jmeter-test.js
+++ test/convert-postman-jmeter-test.js
@@ -1,4 +1,4 @@
-const convertPostmanJmeter = require('../lib/convert-postman-jmeter.js');
+const convertPostmanJmeterTest = require('../lib/convert-postman-jmeter.js');
 const expect = require('chai').expect;
 const fs = require('fs');
 
@@ -7,8 +7,10 @@ describe('Constructor', function() {
     const jmeterProjectFile1 = 'test/resources/test-api-with-out-request.postman_collection.jmx';
     const jmeterProjectFile2 = 'test/resources/test-api-without-environments.postman_collection.jmx';
     const jmeterProjectFile3 = 'test/resources/test-api-without-environments-v9.21.5.postman_collection.jmx';
+    const jmeterProjectFile4 = 'test/resources/test-api-with-environments.postman_collection.jmx';
     const file1 = 'test/resources/batch-projects/test-api-without-environments-1.postman_collection.jmx';
     const file2 = 'test/resources/batch-projects/test-api-without-environments-2.postman_collection.jmx';
+    const file3 = 'test/resources/batch-projects/test-api-with-environments.postman_collection.jmx';
 
     before(function() {
       if (fs.existsSync(jmeterProjectFile1)) {
@@ -20,11 +22,17 @@ describe('Constructor', function() {
       if (fs.existsSync(jmeterProjectFile3)) {
         fs.unlinkSync(jmeterProjectFile3);
       }
+      if (fs.existsSync(jmeterProjectFile4)) {
+        fs.unlinkSync(jmeterProjectFile4);
+      }
       if (fs.existsSync(file1)) {
         fs.unlinkSync(file1);
       }
       if (fs.existsSync(file2)) {
         fs.unlinkSync(file2);
+      }
+      if (fs.existsSync(file3)) {
+        fs.unlinkSync(file3);
       }
     });
 
@@ -38,24 +46,30 @@ describe('Constructor', function() {
       if (fs.existsSync(jmeterProjectFile3)) {
         fs.unlinkSync(jmeterProjectFile3);
       }
+      if (fs.existsSync(jmeterProjectFile4)) {
+        fs.unlinkSync(jmeterProjectFile4);
+      }
       if (fs.existsSync(file1)) {
         fs.unlinkSync(file1);
       }
       if (fs.existsSync(file2)) {
         fs.unlinkSync(file2);
       }
+      if (fs.existsSync(file3)) {
+        fs.unlinkSync(file3);
+      }
     });
 
     it('without options', function() {
-      expect(convertPostmanJmeter.convert()).to.equal(false);
+      expect(convertPostmanJmeterTest.convert()).to.equal(false);
     });
 
     it('with file empty value on options', function() {
-      expect(convertPostmanJmeter.convert({projectPostman: ''})).to.equal(false);
+      expect(convertPostmanJmeterTest.convert({projectPostman: ''})).to.equal(false);
     });
 
     it('without file project postman not exists on options', function() {
-      expect(convertPostmanJmeter.convert({projectPostman: 'not-exists.json'})).to.equal(false);
+      expect(convertPostmanJmeterTest.convert({projectPostman: 'not-exists.json'})).to.equal(false);
     });
 
     it('with file project postman exists on options', function() {
@@ -64,7 +78,7 @@ describe('Constructor', function() {
         projectJmeter: jmeterProjectFile2,
         override: true,
       };
-      expect(convertPostmanJmeter.convert(options)).to.equal(true);
+      expect(convertPostmanJmeterTest.convert(options)).to.equal(true);
     });
 
     it('with file project postman exists on options new version Postman v9.21.5', function() {
@@ -73,7 +87,7 @@ describe('Constructor', function() {
         projectJmeter: jmeterProjectFile3,
         override: true,
       };
-      expect(convertPostmanJmeter.convert(options)).to.equal(true);
+      expect(convertPostmanJmeterTest.convert(options)).to.equal(true);
     });
 
     it('with file project postman without request', function() {
@@ -83,7 +97,7 @@ describe('Constructor', function() {
         override: true,
       };
 
-      expect(convertPostmanJmeter.convert(options)).to.equal(true);
+      expect(convertPostmanJmeterTest.convert(options)).to.equal(true);
       expect(fs.existsSync(jmeterProjectFile1)).to.equal(true);
     });
 
@@ -95,10 +109,20 @@ describe('Constructor', function() {
         batchFolder: 'test/resources/batch-projects',
       };
 
-      expect(convertPostmanJmeter.convert(options)).to.equal(true);
+      expect(convertPostmanJmeterTest.convert(options)).to.equal(true);
 
       expect(fs.existsSync(file1)).to.equal(true);
       expect(fs.existsSync(file2)).to.equal(true);
+    });
+
+    it('with file project postman with environment exists', function() {
+      const options = {
+        projectPostman: 'test/resources/test-api-with-environments.postman_collection.json',
+        projectJmeter: jmeterProjectFile4,
+        override: true,
+        projectEnvironmentPostman: 'test/resources/test-api-environment.postman_environment.json',
+      };
+      expect(convertPostmanJmeterTest.convert(options)).to.equal(true);
     });
   });
 });
